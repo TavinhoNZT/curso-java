6175744e7e595fc4e3b66c9921cab3d4da2d9582
Tavinho NZT <investindosabedoria@gmail.com>
2022-01-13 06:36:27 -0300

    aula

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+   
+        double salario;
+		salario = 1250.70;
+		System.out.println("Meu salario e, " + salario);
+		
+		double idade = 31;
+		System.out.println(idade);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println(novaTentativa);
+   
+   
+    }
+}
+
